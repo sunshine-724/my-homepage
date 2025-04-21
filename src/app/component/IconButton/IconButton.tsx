90c7d9b7294f71a6c27d8bfeb04d6b6727cb0a18
中川一樹 <137146752+sunshine-724@users.noreply.github.com>
2025-04-21 11:19:31 +0900

    ポートフォリオのトップページのテキスト部分がタブレットだとズレていたので修正

--- src/app/component/IconButton/IconButton.tsx
+++ src/app/component/IconButton/IconButton.tsx
@@ -45,37 +45,37 @@ interface IconButtonWithTitleProps extends IconButtonProps {
 
 const IconButtonWithTitle: React.FC<IconButtonWithTitleProps> = ({ titleName, pngPath, handleClick }) => {
   return (
-    <Box
-      sx={{ justifyItems: "center" }}
-    >
-      <Typography
-        sx={{ fontSize: { xs: "1.2rem", sm: "1.4rem", md: "1.6rem", lg: "1.8rem" } }}
-      >{titleName}
-      </Typography>
-      <motion.div
-        viewport={{once:true}} //一度だけ実行
-        initial={{ opacity: 0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          duration: 0.8,
-          ease: "easeOut"
-        }}
+    <motion.div
+      viewport={{ once: true }} //一度だけ実行
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.8,
+        ease: "easeOut"
+      }}
 
-        whileHover={{
-          scale: 1.1,
-          transition:{duration:0.2,ease:"easeOut"}
-        }}
-        whileTap={{
-          scale: 1.1,
-          transition:{duration:0.1,ease:"easeOut"}
-        }}
+      whileHover={{
+        scale: 1.1,
+        transition: { duration: 0.2, ease: "easeOut" }
+      }}
+      whileTap={{
+        scale: 1.1,
+        transition: { duration: 0.1, ease: "easeOut" }
+      }}
+    >
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}
       >
+        <Typography
+          sx={{ fontSize: { xs: "1.2rem", sm: "1.4rem", md: "1.6rem", lg: "1.8rem" } }}
+        >{titleName}
+        </Typography>
         <IconButton
           pngPath={pngPath}
           handleClick={handleClick}
         />
-      </motion.div>
-    </Box>
+      </Box>
+    </motion.div>
   );
 };
 
